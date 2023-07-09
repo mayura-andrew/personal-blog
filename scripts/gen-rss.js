f7d8f46d6441994f5ca846bdfc41d54a40f0cd87
mayura-andrew <mayuraalahakoon@gmail.com>
2023-07-09 13:12:07 +0530

    scholarship vlog

--- scripts/gen-rss.js
+++ scripts/gen-rss.js
@@ -5,9 +5,9 @@ const matter = require('gray-matter')
 
 async function generate() {
   const feed = new RSS({
-    title: 'Your Name',
-    site_url: 'https://yoursite.com',
-    feed_url: 'https://yoursite.com/feed.xml',
+    title: 'Mayura Andrew',
+    site_url: 'www.mayuraalahakoon.me',
+    feed_url: 'https://mayuraalahakoon.me/feed.xml',
   })
 
   const posts = await fs.readdir(path.join(__dirname, '..', 'pages', 'posts'))
