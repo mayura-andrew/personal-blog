7a468d55cdf1bdeca904ae6848a7d785f41a9dd4
mayura-andrew <mayuraalahakoon@gmail.com>
2023-11-15 01:52:58 +0530

    fixed next.config.js

--- next.config.js
+++ next.config.js
@@ -2,26 +2,18 @@ const withNextra = require('nextra')({
   theme: 'nextra-theme-blog',
   themeConfig: './theme.config.js',
   // optional: add `unstable_staticImage: true` to enable Nextra's auto image import
-})
+});
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // any configs you need
-}
 
-module.exports = withNextra(nextConfig)
-
-// next.config.js
-module.exports = {
   images: {
     unoptimized: true,
   },
-};
-
-// next.config.js
-module.exports = {
   basePath: '/mayura_andrew',
   assetPrefix: '/mayura_andrew',
   // ... other configurations
-}
+};
 
+module.exports = withNextra(nextConfig);
