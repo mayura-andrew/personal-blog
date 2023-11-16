06ee257282192934d8239b2fef33951482527214
mayura-andrew <mayuraalahakoon@gmail.com>
2023-11-16 14:39:03 +0530

    updated projects [Knowlihub]

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,14 @@ import 'nextra-theme-blog/style.css'
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
 import '../styles/main.css'
+
+const socialMediaLinks = [
+  { name: 'LinkedIn', url: 'https://www.linkedin.com/in/mayura-alahakoon-827381201/', icon: 'images/linkedin.svg' },
+  { name: 'GitHub', url: 'https://github.com/mayura-andrew', icon: 'images/github.svg' },
+  { name: 'StackOverFlow', url: 'https://stackoverflow.com/users/18835623/mayura-andrew', icon: 'images/stackoverflow.svg'}
+  // Add more social media links as needed
+];
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
@@ -21,8 +29,19 @@ export default function App({ Component, pageProps }: AppProps) {
         />
  
         <meta property="og:title" content="Mayura Andrew" />
-        <meta property="og:image" content="https://mayuraandrew.tech/public/favicon.ico" />
+        <meta property="og:image" content="../public/favicon.ico" />
       </Head>
+            {/* Add social media links to the navigation bar */}
+            <header>
+        <nav>
+          <div className="social-links">
+            {socialMediaLinks.map((link) => (
+              <a key={link.name} href={link.url} target="_blank" rel="noopener noreferrer">
+              <img src={link.icon} alt={link.name} /></a>
+            ))}
+          </div>
+        </nav>
+      </header>
       <Component {...pageProps} />
     </>
   )
